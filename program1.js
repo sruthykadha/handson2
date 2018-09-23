0550e2d01ccd6936cfe858a7d0c1f0442c0d73f7
sruthykadha <42710570+sruthykadha@users.noreply.github.com>
2018-09-23 17:57:13 +0530

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,6 @@
+function tellFortune(jobtitle,geolocation,pname,child){
+    console.log("Iam an "+jobtitle+" in "+geolocation+" and married to " +pname+ " with "+child+ "child");  
+}
+tellFortune("engineer","usa","sharukh",1);
+tellFortune("doctor","london","hanuman",2);
+tellFortune("farmer","vizag","salman",2);
