ffe18e660f108ab65cde9447e288d4d795d8e7d6
sruthykadha <42710570+sruthykadha@users.noreply.github.com>
2018-09-23 17:55:56 +0530

    Add files via upload

---
+++ program5.js
@@ -0,0 +1,10 @@
+function higherNum(x,y){
+    if(x>y)
+    
+    console.log(' The highest number of ' + x +  ' and ' + y +' is  ' + x);
+    
+ else (x<y)
+    console.log(' The highest number of ' + x +  ' and ' + y + ' is ' + y);
+}
+higherNum(28,38);
+higherNum(58,64);
