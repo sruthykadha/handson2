de4b14d0765ce5f91b55a139b1b6799385f0dd18
sruthykadha <42710570+sruthykadha@users.noreply.github.com>
2018-09-24 20:11:21 +0530

    Add files via upload

---
+++ program7.js
@@ -0,0 +1,17 @@
+function assignGrade(nsr){
+if(nsr>85)
+return 'A';
+else if(nsr>70)
+return 'B';
+else if(nsr>55)
+return 'C';
+else if(nsr>40)
+return 'D';
+else 
+return 'F';
+}
+var pass=new console.log('you got a ' + assignGrade(90));
+var pass=new console.log('you got a ' + assignGrade(55));
+var pass=new console.log('you got a ' + assignGrade(60));
+var pass=new console.log('you got a ' + assignGrade(55));
+var pass=new console.log('you got a ' + assignGrade(35));
